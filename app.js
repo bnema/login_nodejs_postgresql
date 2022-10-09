6b173b970ecce17e21b5343002fa0d2e98a51988
Brice Nema <brice@briceamen.com>
2022-10-09 03:15:28 +0200

    PostgresSQL and localtunnel now working

--- app.js
+++ app.js
@@ -1,70 +1,66 @@
-// ---( Serveur Express - NodeJS )---
-// Les modules
+
+// --- Modules import ---
     const express = require('express')
+    // We load our environment variables from the config.env file
+    require('dotenv').config({ path: 'config.env' })
     const app = express()
+    const PORT = process.env.devPORT
     const pg = require('pg')
-    const port = process.env.devPORT
-    //  Connexion à la base de données
+    const localtunnel = require('localtunnel')
     const exphbs = require('express-handlebars')
     const fs = require('fs')
     const readFileSync = require('fs').readFileSync
-    // On charge le fichier de config Express 
-    const env = require('dotenv').config({path: './env/express_config.env'})
-    // On charge le fichier de config de la base de données
-    const db = require('dotenv').config({path: './env/db_config.env'})
-    // On a besoin de la base de donnée PostgreSQL
-    const { Client } = require('pg')
-    const client = new Client({
-        user: process.env.dbUSER,
-        host: process.env.dbHOST,
-        database: process.env.dbDATABASE,
-        password: process.env.dbPASSWORD,
-        port: process.env.dbPORT,
-    })
-// --- Fin des modules ---
 
-
-// ---( PostgresSQL - Connexion )---
-    // On se connecte à la base de données
-    client.connect()
-    // Si la connexion échoue, on affiche l'erreur sinon on affiche un message de succès
-    client.on('error', (err) => {
-        console.log('Erreur de connexion à la base de données', err)
+    // Connection to the database (PostgreSQL)
+    const client = new pg.Client({
+        user : process.env.DB_USER,
+        host : process.env.DB_HOST,
+        database : process.env.DB_NAME,
+        password : process.env.DB_PASS,
+        port : process.env.DB_PORT,
+        // String on one line to avoid errors
+        string: process.env.DATABASE_URL // UNDEFINED !!
     })
-    client.on('connect', () => {
-        console.log('Connexion à la base de données réussie')
-    })
-    // On envoie une requête à la base de données
-    client.query('SELECT NOW()', (err, res) => {
-        console.log(err, res)
-        client.end()
-    })
-// --- Fin de la connexion ---
-
+    // We set up the view engine
+    app.set('view engine', 'handlebars')
+    // We set up the public folder
+    app.use(express.static('./express/src/public'))
+    // We use the views folder for the view files
+    app.set('./src/express/views', 'views')
+// --- End of modules ---
 
 // ---( Express - Configuration )---
-    app.get('/', (req, res) => {
-        res.send('Hello World!')
-        })
-    app.listen(port, () => {
-    console.log(`le serveur tourne sur le port ${port}`)
+// We connect to the database first and then start the server + tunnel
+    client.connect()
+    .then(() => {
+        console.log(`Connected to the database \x1b[33m${process.env.DB_NAME}\x1b[0m with the user\x1b[33m ${process.env.DB_USER}\x1b[0m`)
     })
-// --- Fin de la configuration ---
 
-
-// ---( Lancement du serveur localtunnel)---
-    // When the server is started we start the localtunnel service
-    const localtunnel = require('localtunnel')
-    // and print the public URL to the console with the subdomain from the .env file
-    const tunnel = localtunnel({ port: process.env.devPORT, subdomain: process.env.devSUBDOMAIN }, (err, tunnel) => {
-            // subdomain
+    // Then we start the server
+    app.listen(PORT, () => {
+        console.log(`Server is running on port \x1b[33m${PORT}\x1b[0m`)
+    })
+    // We send "Hello World" to the client
+    app.get('/', (req, res) => {
+        res.send('Hello World')
+    })
+    // Get the array of subdomainsList from the config./env file
+    const subdomainsList = process.env.subdomainsList.split(',')
+    // Log all the subdomains in the console
+    console.log(subdomainsList)
     
+    // Try all the subdomains in the array until one is available
+    subdomainsList.forEach(subdomain => {
+        localtunnel(PORT, { subdomain: subdomain }, (err, tunnel) => {
             if (err) {
-                console.error(err)
-                process.exit(1)
+                console.log(`\x1b[31mError\x1b[0m: ${err}`)
+            } else {
+                console.log(`\x1b[32mSuccess\x1b[0m: Your url is \x1b[33m${tunnel.url}\x1b[0m`)
             }
-
-            console.log(`Tunnel URL: ${tunnel.url}`)
-        }
-    )
-// --- Fin du lancement ---
+            // Close the tunnel when the process is closed
+            tunnel.on('close', () => {
+                console.log(`\x1b[31mTunnel\x1b[0m: closed`)
+            })
+        })
+    })
+// --- End of Express configuration ---
